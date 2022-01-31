83a7365459dfade16a2825991068bc4cf5895c64
Pradip-Karmakar-Dev <pradip.karmakar@aimdek.com>
2022-01-31 17:08:01 +0530

    implemented 2 routes for all users & fetch user using id from users.json file

--- Web Technology/NodeJs Crud Operation/index.js
+++ Web Technology/NodeJs Crud Operation/index.js
@@ -9,11 +9,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-
+    fs.readFile('./users.json', 'utf-8', (err, data) => {
+        if (err) console.log(err)
+        else res.send(JSON.parse(data));
+    });
 });
 
 app.get('/api/users/:id', (req, res) => {
-
+    fs.readFile('./users.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        else {
+            const users = JSON.parse(data);
+            const user = users.find(user => (user.id === parseInt(req.params.id)));
+            if (user) res.send(user);
+            else res.send('No user Found');
+        }
+    });
 });
 
 app.post('/api/users', (req, res) => {
@@ -27,3 +41,6 @@ app.put('/api/users/:id', (req, res) => {
 app.delete('/api/users/:id', (req, res) => {
 
 });
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening to port ${port} ....`));
