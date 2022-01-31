6d7537a5fc5a521dbb7cbddb97e5b002aa025a90
Pradip-Karmakar-Dev <pradip.karmakar@aimdek.com>
2022-01-31 19:25:07 +0530

    implemented post & put routes for the users | also added user input validation using JOI

--- Web Technology/NodeJs Crud Operation/index.js
+++ Web Technology/NodeJs Crud Operation/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -9,38 +10,74 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-    fs.readFile('./users.json', 'utf-8', (err, data) => {
-        if (err) console.log(err)
-        else res.send(JSON.parse(data));
-    });
+    const users = readingFile();
+    res.send(users);
 });
 
 app.get('/api/users/:id', (req, res) => {
-    fs.readFile('./users.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err)
-            return;
-        }
-        else {
-            const users = JSON.parse(data);
-            const user = users.find(user => (user.id === parseInt(req.params.id)));
-            if (user) res.send(user);
-            else res.send('No user Found');
-        }
-    });
+    const users = readingFile();
+    const user = users.find(user => (user.id === parseInt(req.params.id)));
+    if (user) res.send(user);
+    else res.send('No user Found');
 });
 
 app.post('/api/users', (req, res) => {
-
+    const { error } = validateData(req.body);
+    if (error) {
+        res.status(400).send(error);
+        return;
+    }
+    let users = readingFile();
+    users.push(req.body);
+    const updatedObjectList = JSON.stringify(users, null, 2);
+    writingFile(updatedObjectList);
+    res.send(updatedObjectList);
 });
 
 app.put('/api/users/:id', (req, res) => {
-
+    const { error } = validateData(req.body);
+    if (error) {
+        res.status(400).send(error);
+        return;
+    }
+    let users = readingFile();
+    let findUserIndex = users.findIndex((user) => user.id === parseInt(req.body.id));
+    if (findUserIndex != -1) {
+        users[findUserIndex] = req.body;
+    }
+    const updatedObjectList = JSON.stringify(users, null, 2);
+    writingFile(updatedObjectList);
+    res.send(updatedObjectList);
 });
 
 app.delete('/api/users/:id', (req, res) => {
 
 });
+
+function validateData(user) {
+    const schema = Joi.object({
+        id: Joi.number().required(),
+        name: Joi.string().required(),
+        password: Joi.string().min(6).required(),
+        gender: Joi.string().required(),
+        birthdate: Joi.string().required(),
+        age: Joi.number().required(),
+        country: Joi.string().required(),
+        phone: Joi.string().required(),
+    });
+
+    return schema.validate(user);
+}
+
+function readingFile() {
+    return JSON.parse(fs.readFileSync('./users.json'));
+}
+
+function writingFile(updatedFile) {
+    fs.writeFileSync('./users.json', updatedFile, (err) => {
+        if (err) console.log(err)
+    });
+}
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening to port ${port} ....`));
