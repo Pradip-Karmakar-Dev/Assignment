580f828f99def7edf59258bc73283c7461f9ffe3
Pradip-Karmakar-Dev <pradip.karmakar@aimdek.com>
2022-02-01 12:16:23 +0530

    Added routes folder and seperate routes from index.js | Refactor code for better performance

--- Web Technology/NodeJs Crud Operation/index.js
+++ Web Technology/NodeJs Crud Operation/index.js
@@ -1,90 +1,11 @@
-const fs = require('fs');
-const Joi = require('joi');
+const users = require('./routes/users');
+const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Welcome to Aimdek');
-});
-
-app.get('/api/users', (req, res) => {
-    const users = readingFile();
-    res.send(users);
-});
-
-app.get('/api/users/:id', (req, res) => {
-    const users = readingFile();
-    const user = users.find(user => (user.id === parseInt(req.params.id)));
-    if (user) res.send(user);
-    else res.send('No user Found');
-});
-
-app.post('/api/users', (req, res) => {
-    const { error } = validateData(req.body);
-    if (error) {
-        res.status(400).send(error);
-        return;
-    }
-    let users = readingFile();
-    users.push(req.body);
-    const updatedObjectList = JSON.stringify(users, null, 2);
-    writingFile(updatedObjectList);
-    res.send(updatedObjectList);
-});
-
-app.put('/api/users/:id', (req, res) => {
-    const { error } = validateData(req.body);
-    if (error) {
-        res.status(400).send(error);
-        return;
-    }
-    let users = readingFile();
-    let findUserIndex = users.findIndex((user) => user.id === parseInt(req.body.id));
-    if (findUserIndex != -1) {
-        users[findUserIndex] = req.body;
-    }
-    const updatedObjectList = JSON.stringify(users, null, 2);
-    writingFile(updatedObjectList);
-    res.send(updatedObjectList);
-});
-
-app.delete('/api/users/:id', (req, res) => {
-    let users = readingFile();
-    let findUserIndex = users.findIndex((user) => user.id === parseInt(req.body.id));
-    if (findUserIndex != -1) {
-        users.splice(findUserIndex, 1);
-    }
-    const updatedObjectList = JSON.stringify(users, null, 2);
-    writingFile(updatedObjectList);
-    res.send(updatedObjectList);
-});
-
-function validateData(user) {
-    const schema = Joi.object({
-        id: Joi.number().required(),
-        name: Joi.string().required(),
-        password: Joi.string().min(6).required(),
-        gender: Joi.string().required(),
-        birthdate: Joi.string().required(),
-        age: Joi.number().required(),
-        country: Joi.string().required(),
-        phone: Joi.string().required(),
-    });
-
-    return schema.validate(user);
-}
-
-function readingFile() {
-    return JSON.parse(fs.readFileSync('./users.json'));
-}
-
-function writingFile(updatedFile) {
-    fs.writeFileSync('./users.json', updatedFile, (err) => {
-        if (err) console.log(err)
-    });
-}
+app.use('/', home);
+app.use('/api/users/', users);
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening to port ${port} ....`));
---
+++ Web Technology/NodeJs Crud Operation/routes/users.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const fs = require('fs');
+
+router.get('/', (req, res) => {
+    const users = readingFile();
+    res.send(users);
+});
+
+router.get('/:id', (req, res) => {
+    const users = readingFile();
+    const user = users.find(user => (user.id === parseInt(req.params.id)));
+    if (!user) return res.status(404).send('No Such User found for given ID');
+    res.send(user);
+});
+
+router.post('/', (req, res) => {
+    const { error } = validateData(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let users = readingFile();
+    users.push(req.body);
+    const updatedObjectList = JSON.stringify(users, null, 2);
+    writingFile(updatedObjectList);
+    res.send('User Added Succesfully.....');
+});
+
+router.put('/:id', (req, res) => {
+    const { error } = validateData(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let users = readingFile();
+    let foundUserIndex = users.findIndex((user) => user.id === parseInt(req.params.id));
+    console.log(foundUserIndex);
+    if (foundUserIndex == -1) return res.status(404).send('No Such User found for given ID');
+
+    users[foundUserIndex] = req.body;
+    const updatedObjectList = JSON.stringify(users, null, 2);
+    writingFile(updatedObjectList);
+    res.send('User Updated Successfully.....');
+});
+
+router.delete('/:id', (req, res) => {
+    let users = readingFile();
+    let foundUserIndex = users.findIndex((user) => user.id === parseInt(req.params.id));
+    console.log(foundUserIndex);
+    if (foundUserIndex == -1) return res.status(404).send('No Such User found for given ID');
+
+    users.splice(foundUserIndex, 1);
+    const updatedObjectList = JSON.stringify(users, null, 2);
+    writingFile(updatedObjectList);
+    res.send('User Deleted Successfully.....');
+});
+
+function validateData(user) {
+    const schema = Joi.object({
+        id: Joi.number().required(),
+        name: Joi.string().required(),
+        password: Joi.string().min(6).required(),
+        gender: Joi.string().required(),
+        birthdate: Joi.string().required(),
+        age: Joi.number().required(),
+        country: Joi.string().required(),
+        phone: Joi.string().required(),
+    });
+
+    return schema.validate(user);
+}
+
+function readingFile() {
+    return JSON.parse(fs.readFileSync('./users.json'));
+}
+
+function writingFile(updatedFile) {
+    fs.writeFileSync('./users.json', updatedFile, (err) => {
+        if (err) console.log(err)
+    });
+}
+
+module.exports = router;
